f4b67f8bf6f578e0b51399187315359ec46e15c7
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-01-08 19:00:21 +0530

    Add files via upload

---
+++ Pattern17.java
@@ -0,0 +1,21 @@
+public class Pattern17 extends Thread {
+	public void run(){
+		try{
+			for(int i=5; i>=1; i--){
+				for(int j=1; j<=i; j++){
+					System.out.print(j+" ");
+					Thread.sleep(1000);
+				}
+				System.out.println();
+			}
+			
+		} catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	public static void main(String...strings){
+		Pattern17 obj = new Pattern17();
+		Thread th = new Thread(obj);
+		th.start();
+	}
+}
