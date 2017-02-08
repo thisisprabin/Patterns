e0f08ed67610e1adc899e6c05a6cf21d38ecbdda
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-08 12:52:15 +0530

    Add files via upload

---
+++ Pattern type 3/Pattern16.java
@@ -0,0 +1,30 @@
+/*
+	1 1 1 1 1 
+	2 2 2 2 
+	3 3 3 
+	4 4 
+	5 
+*/
+public class Pattern16 extends Thread {	
+	public void run(){
+		try {
+			
+			for(int i=1; i<=5; i++){
+				for(int j=5; j>=i; j--){
+					System.out.print(i+" ");
+					Thread.sleep(1000);
+				}
+				System.out.println();
+			}
+			
+		} catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	public static void main(String...strings){
+		Pattern16 obj = new Pattern16();
+		Thread th = new Thread(obj);
+		th.start();
+	}
+}
